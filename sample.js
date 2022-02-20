f360e6fb1bb5bfb120fbcb51e766d532d8b51d53
nanbanchicken <nanbanchicken@outlook.jp>
2022-02-20 14:49:02 +0900

    ver 4
    ダンジョンのマスの属性(タイル)の概念を追加
    ダンジョンの1次元インデックスからx, yそれぞれ座標を取得する関数を追加
    プレイヤークラスを追加
    プレイヤーは最初に見つかった空マスに配置される

--- sample.js
+++ sample.js
@@ -3,16 +3,23 @@
 // twitter @Suminoprogramm1
 // Nanban and Nishino Junji
 
-///---- 2021/6/26
+///---- 2021/7/10
 
 class Dungeon { 
 
     constructor() {
         this._width = 8;
         this._height = 8;
+
+        this._tile_type = {
+            Air: 0,
+            Wall: 1,
+            Player: 2
+        };
         
         // 0: 空間
         // 1: 壁
+        // 2: プレイヤー
         this.map = [
             1,1,1,1,1,1,1,1,
             1,1,1,1,1,1,1,1,
@@ -36,15 +43,33 @@ class Dungeon {
         }
     }
 
+    // プレイヤーを表示
+    display_player(x, y) {
+        this._draw_tile(x, y, this._tile_type.Player);
+    }
+
     _draw_tile(x, y, tile_type){
-        if(tile_type === 0){
+        if (tile_type == this._tile_type.Air){
             fill(255, 255, 255);
-        } else if(tile_type === 1){
+        } else if (tile_type == this._tile_type.Wall){
             fill(0, 0, 0);
+        } else if (tile_type == this._tile_type.Player) {
+            fill(255, 0, 0);
         }
+        
         rect(x*20, y*20, 20, 20);
     }
-    
+
+    // 1次元の位置を2次元の位置に変換(x)
+    convert_1dTo2d_x(index) {
+        return index % this._width;
+    }
+
+    // 1次元の位置を2次元の位置に変換(y)
+    convert_1dTo2d_y(index) {
+        return Math.floor(index / this._width);
+    }
+
     // 2次元の位置(x, y)を1次元の位置に変換
     _convert_2dTo1d(x, y) {
         return y * this._width + x;
@@ -90,10 +115,46 @@ class Dungeon {
     // mapのx, yの位置に空間(0)を開ける
     _dig_wall(x,  y)  {
         var index = this._convert_2dTo1d(x, y);
-        this.map[index] = 0;
+        this.map[index] = this._tile_type.Air;
+    }
+
+    // 最初の空間座標を取得する
+    get_first_air_index() {
+        for (let i = 0; i < this._width * this._height; i++) {
+            if (this.map[i] == this._tile_type.Air) {
+                return i;
+            }
+        }
     }
     
 }
+//----
+
+class Player {
+
+    constructor(my_dungeon) {
+        this._dungeon = my_dungeon;
+        
+        this._make_player();
+    }
+
+    _make_player() {
+        console.log("make_player");
+        
+        let air_index = this._dungeon.get_first_air_index();
+
+        this._position_x = this._dungeon.convert_1dTo2d_x(air_index);
+        this._position_y = this._dungeon.convert_1dTo2d_y(air_index);
+    }
+    
+    display_player() {
+        console.log("display_player");
+
+        this._dungeon.display_dungeon();
+        this._dungeon.display_player(this._position_x, this._position_y);
+    }
+}
+
 
 //-------
 
@@ -107,6 +168,9 @@ function setup(){
 
     let my_dungeon = new Dungeon();
     my_dungeon.display_dungeon();
+
+    let my_player = new Player(my_dungeon); // 空き部屋の一番左上
+    my_player.display_player();
 }
  
 function draw(){
