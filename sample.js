6f2a0af0f88c4a4614a767343903ebe9d74518ab
nanbanchicken <nanbanchicken@outlook.jp>
2022-02-20 14:42:49 +0900

    ver2
    ダンジョンクラスを追加

--- sample.js
+++ sample.js
@@ -2,51 +2,77 @@
 // https://www.youtube.com/channel/UClZj9tdR1TRkxglnaA55pJA
 // twitter @Suminoprogramm1
 
-let map = [
-    1,1,1,1,1,1,1,1,
-    1,0,0,0,0,0,1,1,
-    1,1,1,1,1,1,1,1,
-    1,0,0,0,1,1,1,1,
-    1,0,0,0,1,1,1,1,
-    1,0,0,0,1,1,1,1,
-    1,1,1,1,1,1,1,1,
-    1,1,1,1,1,1,1,1,
-];
+///----
 
-//map[2]=2;
-//map[1][0][1]
+class Dungeon { 
 
-function map_display(map){
-    for(let y=0; y<8; y++){
-        for(let x=0; x<8; x++){
-            tile_draw(x, y, map_get(x, y));
+    constructor() {
+        // 0: 空間
+        // 1: 壁
+        this.map = [
+            1,1,1,1,1,1,1,1,
+            1,0,0,0,0,0,1,1,
+            1,1,1,1,1,1,1,1,
+            1,0,0,0,1,1,1,1,
+            1,0,0,0,1,0,0,1,
+            1,0,0,0,1,1,1,1,
+            1,1,1,1,1,1,1,1,
+            1,1,1,1,1,1,1,1,
+        ];
+    }
+    
+    display_dungeon()  {
+        console.log("display_dungeon");
+        for(let y=0; y<8; y++){
+            for(let x=0; x<8; x++){
+                this.draw_tile(x, y, this.get_map(x, y));
+            }
         }
     }
-}
 
-function tile_draw(x, y, tile_type){
-    if(tile_type === 0){
-        fill(255, 255, 255);
-    } else if(tile_type === 1){
-        fill(0, 0, 0);
+    draw_tile(x, y, tile_type){
+        if(tile_type === 0){
+            fill(255, 255, 255);
+        } else if(tile_type === 1){
+            fill(0, 0, 0);
+        }
+        rect(x*20, y*20, 20, 20);
     }
-    rect(x*20, y*20, 20, 20);
+
+    get_map(x, y){
+        return (this.map[y*8 + x]);
+    }
+
+    make_dungeon(){
+        console.log("make_dungeon stab");
+    }
+
+    dig_wall(x,y){
+        console.log("dig_wall stab");
+    }
+    
 }
 
-function map_get(x, y){
-    return (map[y*8 + x]);
-}
-
+//-------
 
 let canvasSize = 600;
+//var cellSize = 100;
+//var cellSize;
+//var cellmargin = 2;
+//var cellLength = canvasSize / (cellSize + cellmargin * 2);
+//var cells = [];
 
 function setup(){
     canvasSize=windowHeight;
+//    cellSize = canvasSize/20;
+//    cellLength = canvasSize / (cellSize + cellmargin * 2);
 
     createCanvas(canvasSize, canvasSize);
     background(50, 100, 150);
 
-    map_display(map);
+    let my_dungeon = new Dungeon();
+    //map_display(map);
+    my_dungeon.display_dungeon();
 }
  
 function draw(){
